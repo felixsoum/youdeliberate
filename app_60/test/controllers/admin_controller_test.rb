0e7f8d0a2b226e7daae0c437dbd1afa373412d05
Dong <ldohxc@hotmail.com>
2014-02-06 21:14:59 -0500

    add validation of file type for uploading and corressponding test.

--- app_60/test/controllers/admin_controller_test.rb
+++ app_60/test/controllers/admin_controller_test.rb
@@ -14,8 +14,16 @@ class AdminControllerTest < ActionController::TestCase
   test "should post upload without zip file" do
     post :upload
     assert_response :success
+    assert_template :index
   end
 
+  test "should post upload with non-zip file" do
+    file = fixture_file_upload('files/how-to.pdf', 'application/pdf')
+    post :upload, :narrative => file 
+    assert_response :success
+    assert_template :index
+  end
+  
   test "should post upload with zip file" do
     file = fixture_file_upload('files/1.zip', 'application/zip')
     post :upload, :narrative => file 
