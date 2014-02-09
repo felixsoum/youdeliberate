5dc2a53d41fb0ea3cceac8f8e06d5f52d78a90d9
gchafouleas <genevieve.chafouleas@gmail.com>
2014-02-09 15:18:04 -0500

    bubble transition initial
    we can play around with the values

--- app_60/app/assets/javascripts/bubbles.js
+++ app_60/app/assets/javascripts/bubbles.js
@@ -48,37 +48,46 @@ function refreshBubbles(){
 		.sort(null)
 		.size([diameter, diameter])
 		.padding(1.5);
-
+		
 	var svg = d3.select("#bubbles").append("svg")
 		.attr("width", diameter*2)
 		.attr("height", diameter)
 		.attr("class", "bubble");
 
 	d3.json(fileName, function(error, root) {
-	  var node = svg.selectAll(".node")
+	 var node = svg.selectAll(".node")
 		  .data(bubble.nodes(classes(root))
 		  .filter(function(d) { return !d.children; }))
 		.enter().append("g")
-		  .attr("class", "node")
-		  .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+		  .attr("class", "node");		  
 
 	  node.append("title")
 		  .text(function(d) { return d.className + ": " + format(d.value); });
 
 	  node.append("circle")
+			.attr("r",function(d) {
+		  	 return d.r/2; 
+		  	 })		
+		  .attr("transform", function(d) { return "translate(" + 200 + "," + 200 + ")"; })
+		  .on("mouseover",function(d){d3.select(this).style("fill",function(d) { return getMouseOverColor(d.category); })})
+		  .on("mouseout",function(d){d3.select(this).style("fill", function(d) { return getSectionColor(d.category); })})
+		  .on("click",function(d){$.fancybox({type: 'iframe',href: 'http://localhost:3000/narratives/narrative'});})
+		  .transition()
 		  .attr("r", function(d) {
 		  	 return d.r; 
 		  	 })
+			.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 		  .style("fill", function(d) { return getSectionColor(d.category); })
-		  .on("mouseover",function(d){d3.select(this).style("fill",function(d) { return getMouseOverColor(d.category); })})
-		  .on("mouseout",function(d){d3.select(this).style("fill", function(d) { return getSectionColor(d.category); })})
-		  .on("click",function(d){$.fancybox({type: 'iframe',href: 'http://localhost:3000/narratives/narrative'});});
+		  .style("opacity",0.5)
+		  .duration(4000)
+		  .ease("bounce")
 
 	  node.append("text")
 		  .attr("dy", ".3em")
 		  .style("text-anchor", "middle")
 		  .text(function(d) { return d.className.substring(0,  d.r/ 3); });
-	});
+			
+	});		
 }
 
 // Returns a flattened hierarchy containing all leaf nodes under the root.
