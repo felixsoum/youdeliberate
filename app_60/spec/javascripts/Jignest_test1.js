03f98d0b22d2c40cc2ddc9688491dd44d23260eb
Jignesh Patel <jigneshp23@gmail.com>
2014-03-07 12:45:48 -0500

    Added Jasmine Test

---
+++ app_60/spec/javascripts/Jignest_test1.js
@@ -0,0 +1,13 @@
+//= require sunburst
+
+describe("The getArcMouseOver getter", function(){
+
+it("Calls the getArcMouseOverColor", function(){
+var a;
+
+a = getArcMouseOverColor(0);
+
+expect(a).toEqual("#fff");
+});
+
+});
