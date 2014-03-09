066b7bcf72f6c51835cbb0fbef6404220b1eae72
felixsoum <felix@felixsoum.com>
2014-03-08 23:25:26 -0500

    Refactor flash

--- app_60/app/helpers/application_helper.rb
+++ app_60/app/helpers/application_helper.rb
@@ -1,17 +1,19 @@
 module ApplicationHelper
-  
+
+  # Rails flash messages using Twitter Bootstrap. Read more: https://gist.github.com/roberto/3344628
   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
-  end
-   
-  def flash_messages opts = {}
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
-        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-        concat message
-        end)
+    case flash_type
+      when :success
+        "alert-success" # Green
+      when :error
+        "alert-danger" # Red
+      when :alert
+        "alert-warning" # Yellow
+      when :notice
+        "alert-info" # Blue
+      else
+        flash_type.to_s
     end
-    nil
   end
-  
+
 end
