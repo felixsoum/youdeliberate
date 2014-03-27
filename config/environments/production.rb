30bda87845dd40db8fc09941e487ac40db92602a
Dong <ldohxc@hotmail.com>
2014-03-27 17:43:35 -0400

    Style setting page for admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,22 @@ App60::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # set up smtp server for sending emails.
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "ldohxc@hotmail.com",
+    password: "H28-OD-cVMTJ_bTplKi8-Q"
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
