72ab003d5e85a403f8700ceccdbb496e32487927
felixsoum <felix@felixsoum.com>
2014-04-06 14:02:36 -0400

    Replace é by e

--- app/controllers/narratives_controller.rb
+++ app/controllers/narratives_controller.rb
@@ -75,7 +75,7 @@ class NarrativesController < ApplicationController
   def remove_comment
     narrative_id = params[:id]
     comment = NComment.find(params[:comment_id])
-    comment.update(:content => 'Commentaire supprimé / Comment removed')
+    comment.update(:content => 'Commentaire supprime / Comment removed')
     @comments = get_comments_for_narrative(narrative_id)
     respond_to do |format|
       format.js { render 'comment.js.erb' }
--- test/controllers/narratives_controller_test.rb
+++ test/controllers/narratives_controller_test.rb
@@ -127,6 +127,6 @@ class NarrativesControllerTest < ActionController::TestCase
   
   test "UT-NC-24: Removing a comment simply replaces its text" do
     delete :remove_comment, :format => 'js', id: @narrative.id, comment_id:  @comment.id
-    assert_equal NComment.find(@comment.id).content, 'Commentaire supprimé / Comment removed'
+    assert_equal NComment.find(@comment.id).content, 'Commentaire supprime / Comment removed'
   end
 end
